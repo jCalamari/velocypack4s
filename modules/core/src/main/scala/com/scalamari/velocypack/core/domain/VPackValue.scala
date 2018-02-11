81f0489dcb6a067dabdd45bdd3629d9bb0113d39
jClamari <piotrek.fras@gmail.com>
2018-02-11 00:30:42 +0000

    VMS-1: Add support for primitive types and date

--- modules/core/src/main/scala/com/scalamari/velocypack/core/domain/VPackValue.scala
+++ modules/core/src/main/scala/com/scalamari/velocypack/core/domain/VPackValue.scala
@@ -1,15 +1,34 @@
 package com.scalamari.velocypack.core.domain
 
+import java.util.Date
+
 private[velocypack] sealed trait VPackValue
 
-private[velocypack] final case class VPackObject(fields: List[(String, VPackValue)]) extends
-  VPackValue
+private[velocypack] final case class VPackObject(fields: List[(String, VPackValue)]) extends VPackValue
 
 private[velocypack] final case class VPackArray(items: List[VPackValue]) extends VPackValue
 
 private[velocypack] final case class VPackString(value: String) extends VPackValue
 
-private[velocypack] final case class VPackNumber(value: Double) extends VPackValue
+private[velocypack] final case class VPackDouble(value: Double) extends VPackValue
+
+private[velocypack] final case class VPackInt(value: Int) extends VPackValue
+
+private[velocypack] final case class VPackFloat(value: Float) extends VPackValue
+
+private[velocypack] final case class VPackLong(value: Long) extends VPackValue
+
+private[velocypack] final case class VPackShort(value: Short) extends VPackValue
+
+private[velocypack] final case class VPackChar(value: Char) extends VPackValue
+
+private[velocypack] final case class VPackByte(value: Byte) extends VPackValue
+
+private[velocypack] final case class VPackDate(value: Date) extends VPackValue
+
+private[velocypack] final case class VPackBigDecimal(value: BigDecimal) extends VPackValue
+
+private[velocypack] final case class VPackBigInt(value: BigInt) extends VPackValue
 
 private[velocypack] final case class VPackBoolean(value: Boolean) extends VPackValue
 
--- modules/core/src/main/scala/com/scalamari/velocypack/core/encoder/VPackEncoder.scala
+++ modules/core/src/main/scala/com/scalamari/velocypack/core/encoder/VPackEncoder.scala
@@ -1,6 +1,6 @@
 package com.scalamari.velocypack.core.encoder
 
-import com.scalamari.velocypack.core.domain.VPackValue
+import com.scalamari.velocypack.core.domain.{VPackNull, VPackValue}
 
 private[velocypack] trait VPackEncoder[A] extends Encoder[A, VPackValue] {
 
@@ -14,6 +14,10 @@ private[velocypack] object VPackEncoder {
 
   def createEncoder[A](func: A => VPackValue): VPackEncoder[A] = new VPackEncoder[A] {
     def encode(value: A): VPackValue = func(value)
+  }
+
+  def notNullEncoder[A](func: A => VPackValue): VPackEncoder[A] = new VPackEncoder[A] {
+    override def encode(value: A): VPackValue = Option(value).map(func).getOrElse(VPackNull)
   }
 
 }
